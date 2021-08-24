d505441b6d9b7b8fa5d8b386e5334c467ad6dc44
poonnada <poonnada.apat@cubeofnine.asia>
2021-08-24 22:18:50 +0700

    aadd

--- routers/index_r.go
+++ routers/index_r.go
@@ -1,11 +1,16 @@
 package routers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func InitRouters(r *gin.Engine, db *gorm.DB) {
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "OK"})
+	})
 	api := r.Group("/api")
 
 	SetCollectionRoutes(api, db)
