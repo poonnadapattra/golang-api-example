50e259060faf52db044de4d420cc97bc5aba0e95
poonnada <poonnada.apat@cubeofnine.asia>
2021-08-25 16:33:55 +0700

    [poon] fix port

--- configs/constant.go
+++ configs/constant.go
@@ -1,7 +1,6 @@
 package configs
 
 import (
-	"log"
 	"os"
 
 	"github.com/spf13/viper"
@@ -13,7 +12,7 @@ type DBController struct {
 }
 
 var (
-	PORT           string = ""
+	PORT           string = "9999"
 	DB_NAME        string = ""
 	DB_HOST        string = ""
 	DB_USER        string = ""
@@ -26,24 +25,27 @@ var (
 func InitConstantVariable() {
 	viper.SetConfigFile(".env")
 	err := viper.ReadInConfig()
+
 	if err != nil {
-		log.Fatalf("Error while reading config file %s", err)
+
 	}
 
-	PORT = geVeriable("PORT")
-	DB_HOST = geVeriable("DB_HOST")
-	DB_NAME = geVeriable("DB_NAME")
-	DB_USER = geVeriable("DB_USER")
-	DB_PORT = geVeriable("DB_PORT")
-	DB_PASSWORD = geVeriable("DB_PASSWORD")
-	REDIS_ADDRESS = geVeriable("REDIS_ADDRESS")
-	REDIS_PASSWORD = geVeriable("REDIS_PASSWORD")
+	PORT = geVeriable("PORT", PORT)
+	DB_HOST = geVeriable("DB_HOST", DB_HOST)
+	DB_NAME = geVeriable("DB_NAME", DB_HOST)
+	DB_USER = geVeriable("DB_USER", DB_USER)
+	DB_PORT = geVeriable("DB_PORT", DB_PORT)
+	DB_PASSWORD = geVeriable("DB_PASSWORD", DB_PASSWORD)
+	REDIS_ADDRESS = geVeriable("REDIS_ADDRESS", REDIS_ADDRESS)
+	REDIS_PASSWORD = geVeriable("REDIS_PASSWORD", REDIS_PASSWORD)
 
 }
 
-func geVeriable(key string) string {
+func geVeriable(key string, val string) string {
 	if value, _ := viper.Get(key).(string); value != "" {
 		return value
+	} else if value = os.Getenv(key); value != "" {
+		return value
 	}
-	return os.Getenv(key)
+	return val
 }
