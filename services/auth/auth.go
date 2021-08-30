9ad48685a6afdd4a2d477ce434d7c8f23a24faf2
poonnada <poonnada.apat@cubeofnine.asia>
2021-08-30 20:13:22 +0700

    new generate token

--- services/auth/auth.go
+++ services/auth/auth.go
@@ -7,6 +7,18 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
+const privateKey = `
+-----BEGIN RSA PRIVATE KEY-----
+MIIBOAIBAAJAc7DQV48nYshsJkVmuj0T1JSHlahAPiRY8z9r0DkFppzwO4zrx9uD
+FHcm6j9Kh09XUSht2yDGC6xqWgOtTJnw2QIDAQABAkBwpUT3PIgZAxVq3kB8LmRU
+pJqv+bczyqhhkOslP6Bk7TJ3AFXoPXaevm4EkRaLLANyJAI291Im/AnLfEvwPxXR
+AiEAuJWAoZyFZCVaVLGtOgW9A3CPa0rNdx0cSFpgjgB7ObcCIQCgc6H5ShXTFBCf
+TrQB+4Dn74YSreDKnS3fgEt1hDVp7wIgSDkfIp0myF+hL6Bx4lEaev0Q8O9M4719
+MoZCX22qyZMCIH1spqsWbKUJxEyj2zbJgWTM6gNkBJqd76QMx+/fH1nlAiBCRhY8
+Giv+YJmd+8r6TYJB+Ar1TuP93jmgoy8vKY43+A==
+-----END RSA PRIVATE KEY-----
+`
+
 type Auth interface {
 	GenerateToken(email string, isUser bool) string
 	ValidateToken(encodedToken string) (*jwt.Token, error)
@@ -19,35 +31,40 @@ type authCustomClaims struct {
 }
 
 type jwtServices struct {
-	secretKey string
-	issure    string
+	privateKey string
 }
 
 func JWTAuthService() Auth {
 	return &jwtServices{
-		secretKey: "secret",
-		issure:    "erz",
+		privateKey: privateKey,
 	}
 }
 
 func (service *jwtServices) GenerateToken(email string, isUser bool) string {
+
+	fmt.Println(service.privateKey)
+
+	SigningKey := []byte(privateKey)
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(SigningKey)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	claims := &authCustomClaims{
 		email,
 		isUser,
 		jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(time.Hour * 48).Unix(),
-			Issuer:    service.issure,
 			IssuedAt:  time.Now().Unix(),
 		},
 	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	//encoded string
-	t, err := token.SignedString([]byte(service.secretKey))
+	token, err := jwt.NewWithClaims(jwt.SigningMethodRS256, claims).SignedString(key)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	return t
+
+	return token
 }
 
 func (service *jwtServices) ValidateToken(encodedToken string) (*jwt.Token, error) {
@@ -55,7 +72,7 @@ func (service *jwtServices) ValidateToken(encodedToken string) (*jwt.Token, erro
 		if _, isvalid := token.Method.(*jwt.SigningMethodHMAC); !isvalid {
 			return nil, fmt.Errorf("Invalid token", token.Header["alg"])
 		}
-		return []byte(service.secretKey), nil
+		return []byte(service.privateKey), nil
 	})
 
 }
